aba28f45d9b3817d649dfe2fc302f53af0252d65
unknown <g.anas786@gmail.com>
2023-07-20 22:42:26 +0530

    router setup for navigation

--- client/src/index.js
+++ client/src/index.js
@@ -18,7 +18,7 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
