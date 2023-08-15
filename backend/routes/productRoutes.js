9e523b4914f94485b516f9ba3c663f83a4f664d3
unknown <g.anas786@gmail.com>
2023-08-15 14:21:48 +0530

    controller files created for products logic

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,21 +1,11 @@
 import express from 'express';
 const router = express.Router();
-import asyncHandler from '../middleware/asyncHandler.js';
-import Product from '../models/productModel.js'
+import { getProducts, getProductById } from '../controllers/productController.js';
 
-router.get('/', asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-}));
+router.get('/', getProducts);
+router.get('/:id', getProductById);
 
-router.get('/:id', asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (!product) {
-        // return res.status(404).json({ message: 'Product not found' })
-        res.status(404)
-        throw new Error('Resource not found')
-    }
-    res.json(product);
-}));
+// router.route('/').get(getProducts)
+// router.route('/:id').get(getProductById)
 
 export default router;
