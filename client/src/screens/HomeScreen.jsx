b3f66410662b186b1d7c1e235c30edef4e27a92c
unknown <g.anas786@gmail.com>
2023-08-17 17:42:45 +0530

    single product fetch using redux toolkit and loader

--- client/src/screens/HomeScreen.jsx
+++ client/src/screens/HomeScreen.jsx
@@ -1,6 +1,7 @@
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
 import { useGetProductsQuery } from '../slices/productApiSlice';
+import Loader from '../components/Loader';
 
 const HomeScreen = () => {
   const { data: products, isLoading, isError } = useGetProductsQuery();
@@ -8,7 +9,7 @@ const HomeScreen = () => {
   return (
     <>
       {isLoading ? (
-        <h2>Loading...</h2>
+        <Loader></Loader>
       ) : isError ? (
         <div>{isError?.data?.message || isError.error}</div>
       ) : (
