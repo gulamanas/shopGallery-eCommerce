a38bef5688a6390bb0b46809b0356e446eefea34
Gulam Anas <g.anas786@gmail.com>
2023-08-27 01:11:29 +0530

    basic route and controller setup for users route

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
-import productRoutes from './routes/productRoutes.js'
+import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 const port = process.env.PORT || 5001;
 
 connectDB(); // Connect to MongoDB
@@ -14,9 +15,10 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-app.use('/api/products', productRoutes)
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
