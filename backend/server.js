cf0c89c7d3e4ba91f028ba466c445f8c92ce75ba
Gulam Anas <g.anas786@gmail.com>
2023-09-02 01:55:16 +0530

    Place order screen added and order routes created and integrated with frontend

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 const port = process.env.PORT || 5001;
 
 connectDB(); // Connect to MongoDB
@@ -17,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Cookie parser middleware
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
   res.send('API is running...');
@@ -25,6 +26,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
