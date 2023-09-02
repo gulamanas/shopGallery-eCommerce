e6079ca2dd43ff2fef0632d9676954f987276067
Gulam Anas <g.anas786@gmail.com>
2023-09-02 23:56:23 +0530

    profile page screen

--- client/src/screens/OrderScreen.jsx
+++ client/src/screens/OrderScreen.jsx
@@ -198,12 +198,12 @@ const OrderScreen = () => {
                     <Loader />
                   ) : (
                     <div>
-                      <Button
+                      {/* <Button
                         onClick={onApproveTest}
                         style={{ marginBottom: '10px' }}
                       >
                         Test Pay Order
-                      </Button>
+                      </Button> */}
                       <div>
                         <PayPalButtons
                           createOrder={createOrder}
